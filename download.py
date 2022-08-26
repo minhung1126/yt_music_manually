8682f5c5c1bd55507bd2c5250bdace644f6760e3
Min Hung DeskMeet B660 <minhung1126@gmail.com>
2022-08-26 18:12:57 +0800

    update download.py and yt_music_core

--- download.py
+++ download.py
@@ -1,1 +1,24 @@
+from yt_music_core import yt_music_core
 
+
+def main():
+    downloader = yt_music_core.YtMusicDownloader()
+
+    downloader.preprocedure()
+
+    while True:
+        url = input('請輸入播放清單連結：')
+
+        if 'list=' not in url:
+            print('沒有偵測到播放清單連結，請確認連結中含有list=')
+            continue
+
+        musics = downloader.get_playlist_musics({'URL': url})
+        musics = downloader.parse_all(musics)
+        downloader.download_all_musics(musics)
+
+        downloader.post_procedure()
+
+
+if __name__ == '__main__':
+    main()
